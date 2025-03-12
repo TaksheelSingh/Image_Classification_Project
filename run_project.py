7fd05ca2a15574526c805c16bc7a0267428a2466
TaksheelSingh <131593520+TaksheelSingh@users.noreply.github.com>
2025-03-13 01:38:26 +0530

    Update run_project.py

--- run_project.py
+++ run_project.py
@@ -4,7 +4,7 @@ from flask_app import app
 
 if __name__ == "__main__":
     # Load the TensorFlow/Keras model
-    model_path = "path/to/image_classifier_model.h5"  # Update with your actual model path
+    model_path = "image_classifier_model.h5"  # Update with your actual model path
     model = tf.keras.models.load_model(model_path)
 
     # Set the model in the Flask app context
